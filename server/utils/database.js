3bd12fd8ba7e54eca5d4b58771a7c7084118c808
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-12-17 14:04:49 -0500

    updated url and anon key for supabase

--- server/utils/database.js
+++ server/utils/database.js
@@ -3,15 +3,19 @@ import { createClient } from '@supabase/supabase-js'
 
 dotenv.config();
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
-
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseServiceKey);
-// const connectionString = process.env.PG_URI
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+const connectionString = process.env.PG_URI
 
 // const sql = postgres(connectionString);
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// import meta would be utilized for front end
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
